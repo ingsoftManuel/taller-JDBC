7ced051d6fecc65007f498302736ad33d51b9e13
ingsoftManuel <132092799+ingsoftManuel@users.noreply.github.com>
2024-02-27 08:50:17 -0500

    Add files via upload

---
+++ untitled/src/main/java/application/Main.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[ ]args){
+        String url= "jdbc:mysql://localhost:3306/test";
+        String user= "root";
+        String password = "admin";
+        try (Connection conn = DriverManager.getConnection(url,user,password));
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM productos");
+    }
+}
